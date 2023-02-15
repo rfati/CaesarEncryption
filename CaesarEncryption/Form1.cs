2a281590ee504937bea814fbe956eff11e08e1b6
rfati <isler.rifat@gmail.com>
2023-02-15 12:59:43 +0300

    Add project files.

---
+++ CaesarEncryption/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CaesarEncryption
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        char[] alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+        char[] TextNormal;
+        String cipher;
+        int adim = 2;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = "";
+            TextNormal = textBox1.Text.ToCharArray();
+            for (int i = 0; i < TextNormal.Length; i++)
+            {
+                for (int j = 0; j < alphabet.Length; j++)
+                {
+                    if (TextNormal[i] == alphabet[j])
+                    {
+                        if (j+adim > 25)
+                        {
+                            int a = (j + adim) - 25;
+                            cipher = cipher + alphabet[a];
+
+                        }
+                        else
+                        {
+                            cipher = cipher + alphabet[j+adim];
+                        }
+                    }
+                }
+            }
+
+            textBox2.Text = cipher;
+            cipher = "";
+
+        }
+
+        private void textBoxCount_TextChanged(object sender, EventArgs e)
+        {
+            adim = Convert.ToInt32(textBoxCount.Text)-1;
+        }
+    }
+}
