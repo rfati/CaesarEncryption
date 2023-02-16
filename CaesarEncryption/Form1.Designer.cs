787e47a0aef179e86e4e7227e90c3b024826d60c
rfati <isler.rifat@gmail.com>
2023-02-16 12:37:51 +0300

    fix bugs

--- CaesarEncryption/Form1.Designer.cs
+++ CaesarEncryption/Form1.Designer.cs
@@ -143,7 +143,6 @@ namespace CaesarEncryption
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
